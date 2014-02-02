81bccb284351dc5b10957a144415d4b56d3fc760
Dontfearmistakes <maximebernardo@gmail.com>
2014-02-02 22:24:49 +0100

    Create Event Model

---
+++ JazzInParis/JazzInParis/JIPEvent.h
@@ -0,0 +1,25 @@
+//
+//  JIPEvent.h
+//  JazzInParis
+//
+//  Created by Max on 02/02/14.
+//  Copyright (c) 2014 Max. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface JIPEvent : NSObject
+
+@property (strong, nonatomic) NSNumber *id;
+@property (strong, nonatomic) NSString *type;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSURL *uri;
+@property (strong, nonatomic) NSString *ageRestriction;
+
+@property (nonatomic) CLLocationCoordinate2D location;
+
+@property (strong, nonatomic) NSDate *date;
+
+@end
---
+++ JazzInParis/JazzInParis/JIPEvent.m
@@ -0,0 +1,13 @@
+//
+//  JIPEvent.m
+//  JazzInParis
+//
+//  Created by Max on 02/02/14.
+//  Copyright (c) 2014 Max. All rights reserved.
+//
+
+#import "JIPEvent.h"
+
+@implementation JIPEvent
+
+@end
