2f4040818f90458ef1b6fcad7ffd77e033d9b0fd
Dontfearmistakes <maximebernardo@gmail.com>
2014-02-05 18:14:22 +0100

    Try to make Sections of TableView dynamic
    Unstable code

--- JazzInParis/JazzInParis/JIPUpComingEventsViewController.m
+++ JazzInParis/JazzInParis/JIPUpComingEventsViewController.m
@@ -11,9 +11,12 @@
 #import "JIPConcertDetailsViewController.h"
 
 @interface JIPUpComingEventsViewController ()
-//proprietés privée
+//ici les proprietés sont automatiquement privée
+
 @property (strong, nonatomic) NSDictionary *upcomingEvents;
 @property (strong, nonatomic) NSArray *allNSDates;
+@property (strong, nonatomic) NSArray *concertsOnDay;
+
 //@property = crée var d'instance + getter (return _ivar) + setter _ivar = smthg)
 
 -(NSDateFormatter *)dateFormatter;
@@ -31,13 +34,14 @@
 -(NSDictionary *)upcomingEvents
 {
     NSDate *tomorrow = [NSDate dateWithTimeInterval:(24*60*60) sinceDate:[NSDate date]];
+    NSDate *dayAfterTomorrow = [NSDate dateWithTimeInterval:(2*24*60*60) sinceDate:[NSDate date]];
     
     if (!_upcomingEvents)
     {
         JIPEvent *event1 = [[JIPEvent alloc] initWithID:@1
                                                   name:@"Vampire WeekEnd"
                                               location:CLLocationCoordinate2DMake(-0.1150322,51.4650846)
-                                                  date:tomorrow  ];
+                                                  date:[NSDate date]  ];
         event1.type = @"Concert";
         event1.uri = [NSURL URLWithString:@"www.dontfearmistakes.com"];
         event1.ageRestriction = @"14+";
@@ -49,13 +53,32 @@
         event2.type = @"Concert";
         event2.uri = [NSURL URLWithString:@"www.dontfearmistakes.com"];
         event2.ageRestriction = @"14+";
+        
+        JIPEvent *event3 = [[JIPEvent alloc] initWithID:@3
+                                                   name:@"Bad Plus"
+                                               location:CLLocationCoordinate2DMake(-0.1150322,51.4650846)
+                                                   date:tomorrow];
+        event3.type = @"Concert";
+        event3.uri = [NSURL URLWithString:@"www.dontfearmistakes.com"];
+        event3.ageRestriction = @"14+";
+        
+        JIPEvent *event4 = [[JIPEvent alloc] initWithID:@3
+                                                   name:@"Paris Combo"
+                                               location:CLLocationCoordinate2DMake(-0.1150322,51.4650846)
+                                                   date:dayAfterTomorrow ];
+        event3.type = @"Concert";
+        event3.uri = [NSURL URLWithString:@"www.dontfearmistakes.com"];
+        event3.ageRestriction = @"14+";
 
         //CREATES NSDICT :: DATE : EVENT
-        _upcomingEvents = @{ tomorrow : event1,
-                             [NSDate date] : event2
+        _upcomingEvents = @{
+                             [NSDate date] : @[event1, event2],
+                             tomorrow : @[event3],
+                             dayAfterTomorrow : @[event4]
                             };
+
         //CREATE NSARRAY WITH ALL DATES TO ACCESS EACH EVENT THROUGH self.upcomingEvents[self.allDates[indexPath.row]]
-        self.allNSDates = [_upcomingEvents allKeys];
+        self.allNSDates = [self.upcomingEvents allKeys];
 
     }
     
@@ -80,14 +103,39 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.upcomingEvents.count;
+    switch (section) {
+        case 0:
+            
+            self.concertsOnDay = self.upcomingEvents[self.allNSDates[0]];
+            NSLog(@"number of rows in section %lu : %ld", (long)section, (unsigned long)self.concertsOnDay.count);
+        
+            return self.concertsOnDay.count;
+            break;
+            
+        case 1:
+            self.concertsOnDay = self.upcomingEvents[self.allNSDates[1]];
+            NSLog(@"number of rows in section %lu : %ld", (long)section, (unsigned long)self.concertsOnDay.count);
+
+            return self.concertsOnDay.count;
+            break;
+            
+        default:
+            self.concertsOnDay = self.upcomingEvents[self.allNSDates[1]];
+            return self.concertsOnDay.count;
+            break;
+    }
+
 }
+
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    //ONE SECTION FOR EACH DAY - TO DO !!!
+    //ONE SECTION FOR EACH DAY
+    NSLog(@"number of sections : %lu", (unsigned long)self.upcomingEvents.count);
     return self.upcomingEvents.count;
+    
 }
+
 
 -(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
@@ -97,8 +145,9 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
-    JIPEvent *event = self.upcomingEvents[self.allNSDates[indexPath.row]];
+    JIPEvent *event = self.upcomingEvents[self.allNSDates[indexPath.section]][indexPath.row];
     
     //CELL GETS EVENT.NAME
     cell.textLabel.text = event.name;
