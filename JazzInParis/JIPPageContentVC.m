fb314d5f10b9e0dd310e5bb59d135b90c9983f30
Dontfearmistakes <maximebernardo@gmail.com>
2014-04-16 16:42:48 +0200

    Fix textViewSize
    still incomplete

--- JazzInParis/JIPPageContentVC.m
+++ JazzInParis/JIPPageContentVC.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Max. All rights reserved.
 //
 
+#define padding 20
 #import "JIPPageContentVC.h"
 
 
@@ -27,10 +28,18 @@
     
     [_initialTextView.layer setBorderWidth: 10.0];
     [_initialTextView.layer setBorderColor:[Rgb2UIColor(49, 49, 49) CGColor]];
-
     _initialTextView.layer.cornerRadius = 10.0f;
 
     [_initialTextView setText:_contentText];
+    
+    CGSize textSize = { 260.0, 10000.0 };
+    //retourne width and height de mon bloc de text en fonction de la police
+    CGSize size = [_initialTextView.text sizeWithFont:[UIFont systemFontOfSize:14]
+                                    constrainedToSize:textSize
+                                        lineBreakMode:NSLineBreakByWordWrapping];
+    
+    [_initialTextView setFrame:CGRectMake(padding, padding+5, size.width, size.height+padding)];
+    [_initialTextView sizeToFit];
     
     if (self.pageIndex == 3)
     {
